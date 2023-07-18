d64dd16a0a373a1d12e82f79b7dfbe4c39fe8617
Thaboteebo <thaboteebo01@gmail.com>
2023-07-18 14:59:04 -0700

    Natural.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * print_time_table - Print the times table of the input,
+ * print_time_table - Print the times table of the input.
  * starting with 0.
+ *
  * @n: The value of the times table to be printed.
  */
 
