ccc59d174dcd85e76d2732bcdfeb05ae3ff5f521
Thaboteebo <thaboteebo01@gmail.com>
2023-07-19 09:59:08 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,50 +8,39 @@
 
 void print_times_table(int n)
 {
-	(0);
-}
+	int prod, mult, num;
 
-/**
- * main - Entry point of the program
- * Return: Always 0.
- */
-
-int main(void)
-{
-	int n, j, product;
-
-	if (n <= 0 && n <= 15)
+	if (n <= 15 && n >= 0)
 	{
-		for (i = 0; i <= n; i++)
+		for (num = 0; num <= n; ++num)
 		{
-			_putchar('0')
-				for (j = 1; j <= n; j++)
+			_putchar(48);
+				for (mult = 1; mult <= n; ++mult)
 				{
 					_putchar(',');
 					_putchar(' ');
 
-					product = i * j;
-					if (product  == 99)
+					product = num * mult;
+					if (prod  <= 9)
 					{
 						_putchar(' ');
 					}
-					if (product <= 9)
+					if (prod <= 99)
 					{
 						_putchar(' ');
 					}
-					if (product >= 100)
+					if (prod >= 100)
 					{
-						_putchar((product / 100) + '0');
-						_putchar((product / 10) % 10 + '0');
+						_putchar((product / 100) + 48);
+						_putchar((product / 10) % 10 + 48);
 					}
-					else if (product <= 99 && product >= 10)
+					else if (prod <= 99 && prod >= 10)
 					{
-						_putchar((product / 10) + '0');
+						_putchar((prod / 10) + 48);
 					}
-					_putchar((product / 10) + '0');
+					_putchar((prod / 10) + 48);
 				}
 			_putchar('\n');
 		}
-		return (0);
 	}
 }
