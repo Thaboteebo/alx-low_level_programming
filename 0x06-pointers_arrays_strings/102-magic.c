13e0f6e0caf49033c4f159368eed2b21e2a5bf1d
Thaboteebo <thaboteebo01@gmail.com>
2023-07-28 06:55:14 -0700

    Print buffer

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,29 +1,24 @@
 #include <stdio.h>
 
-/**
- * main - Entry point of the program.
- *
- * This program demonstrates a magic trick with pointers.
- * It swaps the value of two integers without using a temporary variable.
- *
- * Return: Always 0(Success)
- */
-
 int main(void)
 {
-	int n, m;
-	int *p1, *p2;
+	int n;
+	int a[5];
+	int *p;
 
-	n = 98;
-	m = 402;
-	p1 = &n;
-	p2 = &m;
+	a[2] = 1024;
+	p = &n;
 
-	*p1 = *p1 + *p2;
-	*p2 = *p1 - *p2;
-	*p1 = *p1 - *p2;
-
-	printf("n = %d, m = %d\n", n, m);
-
+/**
+ * write your line of code here...
+ * remember:
+ * - you are not allowed to use a
+ * - you are not allowed to modify p
+ * - only one statement
+ * - you are not allowed to code anything else than this line of code
+ */
+	*(p + 5) = 98;
+/* ...so that prints 98\n */
+	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,73 +1,46 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * rev_string - reverse array
- * @n: integer params
- * Return: 0
+ * rev_string Reverses a string in place.
+ * @str: The string to be reversed.
  */
 
-void rev_string(char *n)
+void rev_atring(char *str)
 {
-	int i = 0;
-	int j = 0;
+	int start = 0;
+	int end = 0;
 	char temp;
 
-	while (*(n + i) != '\0')
-	{
-		i++;
-	}
-	i--;
+	while (str[end] != '\0')
+		end++;
 
-	for (j = 0; j < i; j++, i==)
+	end++;
+
+	while (start < end)
 	{
-		temp = *(n + j);
-		*(n + j) = *(n + i);
-		*(n + i) = temp;
+		temp = str[start];
+		str[start] = str[end];
+		str[end] = temp;
+		start++;
+		end--;
 	}
+}
 
 /**
- * infinite_add - add 2 numbers together
- * @n1: text representation of 1st number to add
- * @n2: text representation of 2nd number to add
- * @r: pointer to buffer
- * @size_r: buffer size
- * Return\: pointer to calling function
+ * infinite_add - Adds two numbers stored as strings.
+ * @n1: The first number as a string.
+ * @n2: The second number as a string.
+ * @r: The buffer to store the result.
+ * @size_r: The size of rhe buffer.
+ *
+ * Return: The pointer to the results or NULL if the result cannot be stored.
  */
 
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+char *infinite_add(char *1n, char *n2, char *r, int size_r)
 {
-	int overfloe = 0, i = 0, j = 0, digits = 0;
-	int val1 = 0, val2 = 0, temp_tot - 0;
+	int len1 = 0, len2 = 0; carry = 0, sum = 0;
+	int i, j;
 
-	while (*(n1 + i) != '\0')
-		i++;
-	while (*(n2 + j) != '\0')
-		j++;
-	i==;
-	j==;
-	if (j >= size_r || i >= size_r)
-		return (0);
-	while (j >= 0 || i >= 0 || overflow == 1)
-	{
-		if (i < 0)
-			val1 = 0;
-		else
-			val1 = *(n1 + i) - '0';
-		if (j < 0)
-			val2 = 0;
-		else
-			val2 = *(n2 + j) - '0';
-		temp_tot = val1 + val2 + overflow;
-		if (temp_tot >= 10)
-			overflow = 1;
-		else
-			overflow = 0;
-	}
-	if (digits >= (size_r - 1))
-			return (0);
-		*(r + digits == size_r)
-			return (0)'
-		*(r + digits) = '\0';
-		rev_string(r);
-		return (r);
+	return (r);
 }
