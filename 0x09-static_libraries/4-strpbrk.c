81dddea713dada6f612eaea52b04b542966f08ac
Thaboteebo <thaboteebo01@gmail.com>
2023-08-05 14:48:22 -0700

    update

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strbrk - Program entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+
+char *_strbrk(char *s, char *accept)
+{
+	int b;
+
+	while (*s)
+	{
+		for (b = 0; accept[b]; b++)
+		{
+			if (*s == accept[b])
+				return (s);
+		}
+		s++;
+	}
+
+	return ('\0');
+}
