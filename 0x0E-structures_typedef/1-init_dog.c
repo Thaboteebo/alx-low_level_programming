a9308767f5070d7d8a7e52b4af8c8d875ea1e748
Thaboteebo <thaboteebo01@gmail.com>
2023-08-15 08:26:06 -0700

    Init dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - Initializes a variable of type struct dog
+ * @d: pointer to struct dog to initialize
+ * @name: Name to iniialize
+ * @age: Age to initialize
+ * @owner: Owner to initialize
+ *
+ * Return: None
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -23,11 +23,55 @@ struct dog
 
 typedef struct dog dog_t;
 
+/**
+ * init_dog - Initializes a struct dog
+ * @d: pointer to a struct dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ */
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d)
+
+/**
+ * print_dog - Prints the details of a dog
+ * @d: Pointer to a struct dog
+ */
+
+void print_dog(struct dog *d);
+
+/**
+ * new_dog - Creates a new struct dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ * Return: Pointer to the new dog
+ */
+
 dog_t *new_dog(char *name, float age, char *owner);
+
+/**
+ * free_dog - Frees memory occupied by a dog
+ * @d: Pointer to a struct dog
+ */
+
 void free_dog(dog_t *d);
+
+/**
+ * _strcpy - Copies a string
+ * @dest: Destination buffer
+ * @src: Source string
+ * Return: Pointer to the destination buffer
+ */
+
 char *_strcpy(char *dest, char *src);
+
+/**
+ * _strlen - Computes the length of a string
+ * @s: Input string
+ * Return: Length of the string
+ */
+
 int _strlen(char *s);
 
 #endif
