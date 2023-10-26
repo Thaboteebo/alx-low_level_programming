278a361678c81bd267e1f7cac78f05e91fb8b54c
Thaboteebo <Thaboteebo01@gmail.com>
2023-10-26 14:53:21 -0700

    binary uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,7 +18,7 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	for (a = 0; b[a] != '\0'; a++)
 	{
-		if (b[a] != '0' && b[a] != '1')
+		if (b[a] != '\0' && b[a] != '1')
 			return (0);
 	}
 	for (a = 0; b[a] != '\0'; a++)
